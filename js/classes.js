9eec81ad4741a5e4077e6e8b9f40730e6e528ee7
ALEX SANDRO DE BORBA JUNIOR <alex_borba-junior@estudante.sesisenai.org.br>
2025-08-14 16:12:33 -0300

    lógica de cadastro e login feitas

--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,5 @@
 class Cliente {
-    constructor(user, senha, email, nome, sobrenome, CPF, telefone, endereco, formPag, tipo) {
+    constructor(user, senha, email, nome, sobrenome, CPF, telefone, endereco) {
         this.user = user,
             this.email = email,
             this.senha = senha,
@@ -7,9 +7,7 @@ class Cliente {
             this.sobrenome = sobrenome,
             this.CPF = CPF,
             this.telefone = telefone,
-            this.endereco = endereco,
-            this.formPag = formPag,
-            this.tipo = tipo
+            this.endereco = endereco
 
     }
 }
